6617d63937f75abcb53a6a27483859dabef22f18
Jai Patel <77733219+Ja1Pat3L@users.noreply.github.com>
2023-10-31 22:03:11 +0000

    Testing Streamlit Deployment

---
+++ CodingAI.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to Streamlit!
+
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+    )
+
+st.write(f'You wrote {len(txt)} characters.')
